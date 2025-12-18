feec78ce75548fbf58c946f608f233a382fd562e
Manon Groos <mgroos@f0r3s7.codam.nl>
2025-12-18 10:47:25 +0100

    fixed mirroring issue by changing camera plane coordinates & made cuboids more cube-shaped

--- src/init_player.c
+++ src/init_player.c
@@ -6,7 +6,7 @@
 /*   By: mgroos <mgroos@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 15:38:14 by mgroos            #+#    #+#             */
-/*   Updated: 2025/12/17 15:38:53 by mgroos           ###   ########.fr       */
+/*   Updated: 2025/12/18 10:44:38 by mgroos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,9 @@ static void	set_player_facing(t_player *player, int direction)
 	}
 }
 
-/** Function to store start info in player struct -> if we have a mega-struct, 
- * might be good to store player in there.
+/** Function to store start info in player struct. The direction the player is
+ * facing and the camera plane direction (all in x and y) are stored depending
+ * on the cardinal direction given in the map.
  */
 void	set_up_player(t_player *player, t_map map)
 {
@@ -51,25 +52,25 @@ void	set_up_player(t_player *player, t_map map)
 	if (map.player_dir == 'N')
 	{
 		set_player_facing(player, NORTH);
-		player->camera_plane.x = 1; // i think ? they do 0.66 in example
+		player->camera_plane.x = 0.6;
 		player->camera_plane.y = 0;
 	}
 	if (map.player_dir == 'E')
 	{
 		set_player_facing(player, EAST);
 		player->camera_plane.x = 0;
-		player->camera_plane.y = 1;
+		player->camera_plane.y = 0.6;
 	}
 	if (map.player_dir == 'S')
 	{
 		set_player_facing(player, SOUTH);
-		player->camera_plane.x = 1;
+		player->camera_plane.x = -0.6;
 		player->camera_plane.y = 0;
 	}
 	if (map.player_dir == 'W')
 	{
 		set_player_facing(player, WEST);
 		player->camera_plane.x = 0;
-		player->camera_plane.y = 1;
+		player->camera_plane.y = -0.6;
 	}
 }
